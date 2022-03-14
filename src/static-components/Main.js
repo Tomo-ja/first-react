4abb531c9f6401f4f304aa0c87dc594a73283043
Tomo-ja <tomonao.hj@gmail.com>
2022-03-13 17:12:00 -0700

    design main contents

--- src/static-components/Main.js
+++ src/static-components/Main.js
@@ -1,14 +1,14 @@
 function Main (){
 	return(
-		<main>
+		<main className="container">
 			<h1>Fun facts about React</h1>
-			<ol>
+			<ul>
 				<li>Was first released in 2013</li>
 				<li>Was originally created by Jordan Walke</li>
 				<li>Has well over 100K stars on GitHub</li>
 				<li>Is maintained by Facebook</li>
 				<li>Powers thousands of enterprise apps, including mobile apps</li>
-			</ol>
+			</ul>
 		</main>
 	)
 }
